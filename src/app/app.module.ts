211d5dc23bea8dd360bdb74250e34e1354f7cecd
Marta Molina <marta.molina@live.u-tad.com>
2022-03-17 11:36:09 +0100

    Generating basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MoviesListComponent } from './views/movies-list/movies-list.component';
+import { MovieCardComponent } from './views/moviesList/movie-card/movie-card.component';
+import { MovieDetailComponent } from './views/movie-detail/movie-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MoviesListComponent,
+    MovieCardComponent,
+    MovieDetailComponent
   ],
   imports: [
     BrowserModule,
